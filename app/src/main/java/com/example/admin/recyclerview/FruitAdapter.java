cc7f3561cdc0510ce74a0adb495765d7db694c7f
huangchixing2 <15007100341@163.com>
2019-03-27 18:32:19 +0800

    recyclerview的点击事件

--- app/src/main/java/com/example/admin/recyclerview/FruitAdapter.java
+++ app/src/main/java/com/example/admin/recyclerview/FruitAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -25,7 +26,25 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
 
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fruit_item, viewGroup, false);
-        ViewHolder holder = new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        //recyclerView的点击事件
+        holder.fruitName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int positon = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(positon);
+                Toast.makeText(v.getContext(), "you clicked view " + fruit.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        holder.fruitImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int postion = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(postion);
+                Toast.makeText(v.getContext(), "you clicked image " + fruit.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
 
         return holder;
     }
